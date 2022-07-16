d3ee9ecc2fa5b38e81d1dbcc4f62cfd0b8d18c9b
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2022-07-16 12:15:26 -0400

    single script to run all sets of models

--- ultrapollution.R
+++ ultrapollution.R
@@ -2,7 +2,7 @@
 
 # Specifying the directory for the data and results -- ensure that the directory is correct
 
-username <- ''
+username <- 'Michael'
 direc <- paste('C:/Users/', username, '/Documents/Data/ultrapollution/ultra_data/', sep = '')
 direc2 <- paste('C:/Users/', username, '/Documents/Data/ultrapollution/results/', sep = '')
 
@@ -12,10 +12,7 @@ diRectoRy <- paste('C:/Users/', username, '/Documents/', sep = '')
 
 # Running the R scripts
 
-source(paste(diRectoRy, 'ultrapollution_PM25.R', sep = ''))
-source(paste(diRectoRy, 'ultrapollution_PM10.R', sep = ''))
-source(paste(diRectoRy, 'ultrapollution_O3.R', sep = ''))
-source(paste(diRectoRy, 'ultrapollution_CO.R', sep = ''))
-source(paste(diRectoRy, 'ultrapollution_NO2.R', sep = ''))
 source(paste(diRectoRy, 'ultrapollution_complete.R', sep = ''))
+source(paste(diRectoRy, 'ultrapollution_events.R', sep = ''))
+source(paste(diRectoRy, 'ultrapollution_gender.R', sep = ''))
 
